8a6d0bb7789951928dfd98986f72902aaa0ca849
Pascal Zarrad <p.zarrad@outlook.de>
2021-12-01 19:44:38 +0100

    feat: remove input files from repository

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,48 @@
+package day1
+
+import printResult
+import readInput
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increments = 0;
+
+        if (input.size < 2) {
+            return increments;
+        }
+
+        for (i in 1 until input.size) {
+            if (input[i].toInt() > input[i- 1].toInt()) {
+                increments++;
+            }
+        }
+
+        return increments;
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var increments = 0;
+
+        if (input.size < 2) {
+            return increments;
+        }
+
+        for (i in 1 until input.size) {
+            if (input[i].toInt() > input[i- 1].toInt()) {
+                increments++;
+            }
+        }
+
+        return increments;
     }
 
-    // test if implementation meets criteria from the description, like:
+    // Check implementation using provided example
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
+    // Output challenge solution
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    printResult(1, part1(input))
+    printResult(2, part2(input))
 }
