0f9dd9f84c94ee193534a867bb276f44db66a9de
Pascal Zarrad <p.zarrad@outlook.de>
2021-12-01 20:03:11 +0100

    feat: add function for default output format

--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,8 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+/**
+ * Output a single result preformatted
+ */
+fun <T> printResult(part: Int, result: T) = println(String.format("Part %d result is: %s", part, result.toString()))
